ceea7d941dee7f8c5555591d9117962f1aafd42c
Harrykuo1 <35657028+Harrykuo1@users.noreply.github.com>
2019-04-12 01:20:47 +0800

    Create 37.cpp

---
+++ 37.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+	int n,ary[100001],upper=0,lower=0,a;
+	scanf("%d",&n);
+	while(n--){
+		scanf("%d",&a);
+		if(a==1){
+			scanf("%d",&a);
+			ary[upper]=a;
+			upper++;
+		}
+		else{
+			if(upper==lower){
+				printf("empty!\n");
+			}
+			else{
+				printf("%d\n",ary[lower]);
+				lower++;
+			}
+		}
+	}
+} 
